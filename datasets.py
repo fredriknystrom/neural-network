e88336c86e5ba2e709774c468275772e2dfe6b9b
fredriknystrom <fredriknystroms@gmail.com>
2024-04-10 23:41:32 +0200

    added or, and, nand dataset

--- datasets.py
+++ datasets.py
@@ -1,6 +1,50 @@
 import numpy as np
 
+train_data = np.array(
+    [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1]])
+
+target_xor = np.array(
+    [
+        [0],
+        [1],
+        [1],
+        [0]])
+
+target_nand = np.array(
+    [
+        [1],
+        [1],
+        [1],
+        [0]])
+
+target_or = np.array(
+    [
+        [0],
+        [1],
+        [1],
+        [1]])
+
+target_and = np.array(
+    [
+        [0],
+        [0],
+        [0],
+        [1]])
+
+def or_dataset():
+    return train_data, target_or
+
 def xor_dataset():
-    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    Y = np.array([[0], [1], [1], [0]])
-    return X, Y
+    return train_data, target_xor
+
+def and_dataset():
+    return train_data, target_and
+
+def nand_dataset():
+    return train_data, target_nand
+
+
